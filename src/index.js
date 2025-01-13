36a3b071631afd7672ddf83745878d7ca1381456
devIncogni <arinkumar10@outlook.com>
2025-01-13 15:53:20 +0530

    - added an icon for future use - styled the scroll bar - set height for nav bars - made further progress on side nav bar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./default-reset.css";
 import "./normal-styles.css";
-import "./header-styles.css"
+import "./header-styles.css";
+import "./side-nav-bar-styles.css";
+import "./scroll-bar-styles.css";
 // import "./menu.svg";
