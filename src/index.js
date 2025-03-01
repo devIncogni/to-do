e03ac0e44f330b60b621be6165181b30b2555ae8
devIncogni <arinkumar10@outlook.com>
2025-03-01 13:38:42 +0530

    styled the task-details side bar

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import "./header-styles.css";
 import "./side-nav-bar-styles.css";
 import "./scroll-bar-styles.css";
 import "./main-content-styles.css";
+import "./task-details-styles.css";
 
 // Asset Imports
 import menuImage from "./menu.svg";
 import sunImage from "./sun-clock-outline.svg";
 import "./circle-outline.svg";
 import "./star-outline.svg";
+import "./calendar-month-outline.svg";
+import "./close.svg";
 
 // JS Imports
 import { ToDo } from "./to-do.js";
@@ -28,3 +31,13 @@ console.log(MyDay.taskList[0]);
 
 AllTasks.removeFromTaskList(abc);
 console.log(AllTasks.taskList[0]);
+
+const datePickerTrigger = document.querySelector(".set-due-date");
+const dateInput = document.querySelector("#to-do-due-date");
+
+console.log(datePickerTrigger);
+console.log(dateInput);
+
+datePickerTrigger.addEventListener("click", () => {
+  dateInput.showPicker();
+});
